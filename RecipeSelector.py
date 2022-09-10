76701425b0bbfac0cd2982fe78b3416691756aa9
amberlai1 <100531457+amberlai1@users.noreply.github.com>
2022-09-10 10:16:12 -0400

    Update RecipeSelector.py
    adding search criteria

--- RecipeSelector.py
+++ RecipeSelector.py
@@ -1,14 +1,17 @@
 import requests
 
+url = "https://yummly2.p.rapidapi.com/feeds/search"
 
-
-
-
-
-url = "https://yummly2.p.rapidapi.com/feeds/auto-complete"
-
-querystring = {"q":"chicken soup"}
-
+ingredients = input("What ingredients do you want to use up? ")
+diet = input("Do you have any dietary restrictions? If so, please type it in! Examples include vegetarian, vegan, lacto... etc ")
+if diet == "No":
+    diet1 = ""
+else:
+    diet1 = diet
+# querystring = {"q":ingredients}
+querystring = {"start":"0","maxResult":"18","maxTotalTimeInSeconds":"7200",
+               "q":ingredients, "allowedAttribute": diet1, "FAT_KCALMax":"1000"}
+# "diet-lacto-vegetarian,diet-low-fodmap","FAT_KCALMax":"1000"
 headers = {
 	"X-RapidAPI-Key": "b913050d98mshda76f7dc59f7480p18af0djsna972adde7447",
 	"X-RapidAPI-Host": "yummly2.p.rapidapi.com"
@@ -16,4 +19,6 @@ headers = {
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 
+print(response.content)
+print(response.headers)
 print(response.text)
